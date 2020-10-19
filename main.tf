da67ada19d1a07c29d26f9568413df78a66f17f0
Ozzy <oaluyi@yahoo.co.uk>
2020-10-19 18:34:17 +0100

    added user_data and new Security Group

--- main.tf
+++ main.tf
@@ -9,6 +9,10 @@ resource "aws_instance" "Terraform" {
  instance_type  = "t2.micro"
  key_name       = "devops"
  subnet_id      = "subnet-8daee3e4"
+ user_data = <<- EOF
+  #!/bin/bash
+  echo "Welcome to DevOps"
+  nohup busybox httpd -f -p 8080 & EOF
 tags = {
         Name = "Terraform_instance"
 }
@@ -17,3 +21,15 @@ resource          "aws_eip" "ip" {
   instance        = "i-066beb58175e2acf5"
   vpc 		= true
 }
+
+resource "aws_security_group" "SG"
+        name = "Terraform-SG"
+
+        ingress {
+                from_port = 8080
+                to_port   = 8080
+                protocol  = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+        }
+        }
+        
